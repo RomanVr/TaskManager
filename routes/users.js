c65cc5ec526013e9cbc8f5c9bceb26aa9a10b8bd
RomanVr <romakf99@yandex.ru>
2019-04-09 20:32:39 +0300

    Add edit user and session control

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,3 @@
-import _ from 'lodash';
 import models from '../models';
 import buildFormObj from '../lib/formObjectBuilder';
 
@@ -25,12 +24,19 @@ export default (router) => {
         ctx.flash.set('User has been created');
         ctx.redirect(router.url('root'));
       } catch (e) {
-        console.log(`error new User: ${JSON.stringify(_.groupBy(e.errors, 'path').email)}`);
+        // console.log(`error new User: ${JSON.stringify(_.groupBy(e.errors, 'path').email)}`);
         ctx.render('users/new', { f: buildFormObj(user, e) });
       }
     })// удаление
     .delete('deleteUser', '/users/:id', async (ctx) => {
+      const userIdsession = ctx.session.userId;
+      // console.log('Session userId: ', userIdsession);
       const { id: userId } = ctx.params;
+      if (!userIdsession || userIdsession.toString() !== userId) {
+        ctx.flash.set('You need to autenticate!');
+        ctx.redirect(router.url('root'));
+        return;
+      }
       const user = await models.User.findOne({
         where: {
           id: userId,
@@ -56,17 +62,31 @@ export default (router) => {
       ctx.render('users/edit', { f: buildFormObj(user) });
     })// редактирование
     .patch('editUserPatch', '/users/:id', async (ctx) => {
+      const userIdsession = ctx.session.userId;
+      // console.log('Session userId: ', userIdsession);
       const { id: userId } = ctx.params;
+      if (!userIdsession || userIdsession.toString() !== userId) {
+        ctx.flash.set('You need to autenticate!');
+        ctx.redirect(router.url('root'));
+        return;
+      }
       const { request: { body: { form } } } = ctx;
-      const userUpdated = await models.User.findOne({
+      const user = await models.User.findOne({
         where: {
           id: userId,
         },
-      })
-        .then((user) => {
-          user.update(form);
-        });
-      console.log(`user for path: ${JSON.stringify(userUpdated)}`);
-      ctx.redirect(router.url('root'));
+      });
+      console.log('/////////////////////////');
+      console.log(`user for path: ${JSON.stringify(user)}`);
+      console.log('/////////////////////////');
+      try {
+        await user.update(form);
+        console.log('Update success!');
+        ctx.flash.set('Has been updated');
+        ctx.redirect(router.url('root'));
+      } catch (e) {
+        // console.log(e.message);
+        ctx.render('users/edit', { f: buildFormObj(user, e) });
+      }
     });
 };
