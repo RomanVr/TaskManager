d8c6af6d933b3feeb86593a11a6290315abeb54a
Roman Vr <romakf99@yandex.ru>
2019-04-15 16:11:12 +0300

    add library jquery-mask-plagin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import 'bootstrap/dist/js/bootstrap.min';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery-ujs';
+import 'jquery-mask-plugin';
+
+// import $ from 'jquery';
+//
+// $(document).ready(() => {
+//   $('#email').mask('#.##', {
+//     recursive: true,
+//     translation: { A: { pattern: /[a-z]/, recursive: true } },
+//   });
+//   $('.ip_address')
+//     .mask('0ZZ.0ZZ.0ZZ.0ZZ', { translation: { Z: { pattern: /[0-9]/, optional: true } } });
+// });
